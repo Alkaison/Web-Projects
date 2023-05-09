2895659596bb8fff28a7c614d8657dfb7967d8b2
Alkaison <505ganeshmourya@gmail.com>
2023-05-09 22:53:46 +0530

    Added: webProjects - NavBar work

---
+++ script.js
@@ -0,0 +1,6 @@
+// Open GitHub on click over profile picture 
+const profilePic = document.getElementById("profilePic");
+
+profilePic.addEventListener("click", () => {
+    window.open("https://github.com/Alkaison");
+});
