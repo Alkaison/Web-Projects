dff7516185793d2a5832dbe247f2aa306116677a
Alkaison <505ganeshmourya@gmail.com>
2023-05-21 22:32:39 +0530

    Feat: open mainPage om title click

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const profilePic = document.getElementById("profilePic");
 profilePic.addEventListener("click", () => {
     window.open("https://github.com/Alkaison");
 });
+
+// Open main page on click over pageTitle 
+const nameTitle = document.getElementById("nameTitle");
+nameTitle.addEventListener("click", () => {
+    window.location.href = "https://alkaison.github.io/Web-Projects";
+});
