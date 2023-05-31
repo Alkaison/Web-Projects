f2746b81df7b35c4985699de8ce68c0d44e158c0
Alkaison <505ganeshmourya@gmail.com>
2023-05-31 21:16:12 +0530

    style: comments and js file path

--- Expanding Cards/script.js
+++ Expanding Cards/script.js
@@ -1,15 +1,17 @@
-const PANEL = document.querySelectorAll(".panel");
+const panels = document.querySelectorAll(".panel");
 
-PANEL.forEach((panel) => {
+// check for "click" event, add "active" class 
+panels.forEach((panel) => {
     panel.addEventListener("click", () => {
         removeActivePanel();
         panel.classList.add("active");
     });
 });
 
+// remove "active" class from all 
 function removeActivePanel()
 {
-    PANEL.forEach((panel) => {
+    panels.forEach((panel) => {
         panel.classList.remove("active");
     });
 }
