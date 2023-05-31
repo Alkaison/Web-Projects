1a99aaacdb220953517bde014eb3640093b9a2c2
Alkaison <505ganeshmourya@gmail.com>
2023-05-31 21:25:29 +0530

    style: comments and formatting.

--- Hidden Search Widget/script.js
+++ Hidden Search Widget/script.js
@@ -2,6 +2,7 @@ const search = document.querySelector('.search');
 const btn = document.querySelector('.btn');
 const input = document.querySelector('.input');
 
+// check for "click" event and toggle active class 
 btn.addEventListener('click', () => {
     search.classList.toggle('active');
     input.focus();
