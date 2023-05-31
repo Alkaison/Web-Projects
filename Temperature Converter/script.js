ae1b278ed52b031f3ed8b232b13e067340037fad
Alkaison <505ganeshmourya@gmail.com>
2023-05-31 21:50:15 +0530

    Refactor: JavaScript code for Temperature Converter.

--- Temperature Converter/script.js
+++ Temperature Converter/script.js
@@ -1,20 +1,10 @@
-const fahrenheitBox = document.getElementById("fahrenheit");
-const celsiusBox = document.getElementById("celsius");
+const btn1 = document.querySelector(".calculateBtn1");
+const btn2 = document.querySelector(".calculateBtn2");
+const fahrenheitBox = document.querySelector("#fahrenheit");
+const celsiusBox = document.querySelector("#celsius");
 
-function calculateCelsius() {
-    const fahrenheit = Number.parseInt(fahrenheitBox.value);
-    let celsius = 0;
-
-    if (fahrenheit === '' || isNaN(fahrenheit) || typeof fahrenheit === 'undefined' || !Number.isInteger(Number(fahrenheit)))
-        fahrenheitBox.style.border = "2px solid red";
-    else {
-        fahrenheitBox.style.border = "2px solid gray";
-        celsius = (fahrenheit - 32) / 1.8;
-        celsiusBox.value = celsius.toFixed(2) + "°C";
-    }
-}
-
-function calculateFahrenheit() {
+// To Fahrenheit 
+btn1.addEventListener("click", () => {
     const celsius = Number.parseInt(celsiusBox.value);
     let fahrenheit = 0;
 
@@ -25,4 +15,18 @@ function calculateFahrenheit() {
         fahrenheit = (celsius * 1.8) + 32;
         fahrenheitBox.value = fahrenheit.toFixed(2) + "°F";
     }
-}
+});
+
+// To Celsius 
+btn2.addEventListener("click", () => {
+    const fahrenheit = Number.parseInt(fahrenheitBox.value);
+    let celsius = 0;
+
+    if (fahrenheit === '' || isNaN(fahrenheit) || typeof fahrenheit === 'undefined' || !Number.isInteger(Number(fahrenheit)))
+        fahrenheitBox.style.border = "2px solid red";
+    else {
+        fahrenheitBox.style.border = "2px solid gray";
+        celsius = (fahrenheit - 32) / 1.8;
+        celsiusBox.value = celsius.toFixed(2) + "°C";
+    }
+});
