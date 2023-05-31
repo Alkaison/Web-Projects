4c0da4c5a376d6fea43a4f9e1a7e3f63dbf2a59d
Alkaison <505ganeshmourya@gmail.com>
2023-05-31 21:03:29 +0530

    style: comments and src changes.

--- Accoridan/script.js
+++ Accoridan/script.js
@@ -1,10 +1,12 @@
 const accordians = document.querySelectorAll(".accordian");
 
+// iterate over each accoridan FAQ and look for "click" event 
 accordians.forEach((accordian) => {
     const icon = accordian.querySelector(".icon");
     const answer = accordian.querySelector(".answer");
 
     accordian.addEventListener("click", () => {
+        // if contains active class, remove and change its height ... else add active class and adjust height 
         if(icon.classList.contains("active")) {
             icon.classList.remove("active");
             answer.style.maxHeight = null;
