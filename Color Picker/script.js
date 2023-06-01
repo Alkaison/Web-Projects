edf91e0e29d889fc644d7d011161f5130250f2c1
Alkaison <505ganeshmourya@gmail.com>
2023-06-01 22:52:04 +0530

    Feat: color Picker

---
+++ Color Picker/script.js
@@ -0,0 +1,39 @@
+const rangeBtns = document.querySelectorAll(".rangeBtn");
+const colorDisplay = document.querySelector(".colorDisplay");
+const copyBtn = document.querySelector("#colorCode");
+const copyCodeBtn = document.querySelector("#copyBtn");
+const body = document.querySelector("body");
+
+// for storing the updated color codes 
+let colorCode;
+
+// look for the input changes in "range" buttons 
+rangeBtns.forEach((rangeBtn) => {
+    rangeBtn.addEventListener("input", () => {
+        updateColorDisplayed();
+    });
+});
+
+// when clicked on the code, copy it to the user clipboard 
+copyCodeBtn.addEventListener("click", () => {
+    navigator.clipboard.writeText(colorCode);
+});
+
+// update the colorCode and set it to display 
+const updateColorDisplayed = () => {
+
+    colorCode = "rgb(";
+    colorCode += rangeBtns[0].value;
+    colorCode += ", ";
+    colorCode += rangeBtns[1].value;
+    colorCode += ", ";
+    colorCode += rangeBtns[2].value;
+    colorCode += ")";
+
+    copyBtn.textContent = colorCode;
+    colorDisplay.style.backgroundColor = colorCode;
+    body.style.backgroundColor = colorCode;
+}
+
+// call for first time check 
+updateColorDisplayed();
