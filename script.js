564ba063478b0ecb786aecbcb668ff6a6b80fb94
Alkaison <505ganeshmourya@gmail.com>
2023-06-07 20:37:40 +0530

    Feat: fetching data from JSON and adding it to DOM

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
-// Open GitHub on click over profile picture 
-const profilePic = document.getElementById("profilePic");
-
-profilePic.addEventListener("click", () => {
-    window.open("https://github.com/Alkaison");
-});
-
 // Open main page on click over pageTitle 
-const nameTitle = document.getElementById("nameTitle");
-nameTitle.addEventListener("click", () => {
+const navTitle = document.querySelector(".navTitle");
+
+navTitle.addEventListener("click", () => {
     window.location.href = "https://alkaison.github.io/Web-Projects";
 });
+
+const projectCardContainer = document.querySelector(".project-card");
+const projectContainer = document.querySelector(".projects-container");
+
+// fetch data from projects.json 
+async function fetchData(path)
+{
+    try {
+        const jsonData = await fetch(path);
+    
+        if(!jsonData.ok)
+            throw new Error(`HTTP error! Status: ${jsonData.status}`);
+    
+        const data = await jsonData.json();
+        return data;
+    }
+    catch(error) {
+        throw error;
+    }
+}
+
+function getProjects()
+{
+    fetchData("./projects.json")
+        .then(data => {
+
+            data.projects.forEach(project => {
+                const projectCard = projectCardContainer.cloneNode(true);
+
+                const pName = projectCard.querySelector(".project-title");
+                pName.textContent = project.name;
+
+                const pLink = projectCard.querySelector("a");
+                pLink.href = project.link;
+
+                const pImage = projectCard.querySelector(".project-image");
+                pImage.src = project.image;
+
+                projectContainer.append(projectCard);
+
+            });
+        })
+        .catch(error => console.log(`Additional information about error: ${error}`));
+}
+
+getProjects();
